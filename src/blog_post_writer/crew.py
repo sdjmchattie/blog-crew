f839c38e723c0ba46c818de367adb65a6f88edb8
Stuart McHattie <stuart.mchattie@gmail.com>
2025-05-16 21:29:35 +0100

    Make the crew act heirarchically

--- src/blog_post_writer/crew.py
+++ src/blog_post_writer/crew.py
@@ -1,4 +1,4 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import LLM, Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
@@ -16,7 +16,7 @@ class BlogPostWriter():
     # Learn more about YAML configuration files here:
     # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
     # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
+
     # If you would like to add tools to your agents, you can learn more about it here:
     # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
@@ -55,10 +55,18 @@ class BlogPostWriter():
         # To learn how to add knowledge sources to your crew, check out the documentation:
         # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
+        llm = LLM(
+            model="ollama/llama3.1:8b",
+            temperature=0.7,
+            base_url="http://localhost:11434",
+        )
+
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            process=Process.hierarchical,
+            planning=True,
+            manager_llm=llm,
+            planning_llm=llm,
         )
